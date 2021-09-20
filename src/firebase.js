f34ce902860501ef61fe1badb07d11e70f2f4c8f
Diego Padilla <61509950+dieegopa@users.noreply.github.com>
2021-09-20 14:17:56 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHDlKbI0bEEvEztlFgMHh0Bfjjcbbz-Hk",
-    authDomain: "chat-udemy-react.firebaseapp.com",
-    projectId: "chat-udemy-react",
-    storageBucket: "chat-udemy-react.appspot.com",
-    messagingSenderId: "106908414933",
-    appId: "1:106908414933:web:25f88048894b09caad035c"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
